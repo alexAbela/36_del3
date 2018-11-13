cc57becca711e16ef2c3bc05595d1f97ade58307
alexAbela <s175124@student.dtu.dk>
2018-11-13 11:57:44 +0100

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+
+    private String name;
+    private BankAccount account;
+
+
+    public Player(String name) {
+
+        this.name = name;
+        this.account = new BankAccount(0);
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+
+    public BankAccount getAccount() {
+        return account;
+    }
+}
